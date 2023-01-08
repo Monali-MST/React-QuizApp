b89e2fd156f7cf63ed4119eaaec9f559cd3fe02e
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-01-08 17:23:48 +0530

    add

---
+++ quizapp/src/Components/MainMenu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MainMenu() {
+  return (
+    <div>
+      MainnnnnMenu
+    </div>
+  )
+}
---
+++ quizapp/src/Components/Quiz.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Quiz() {
+  return (
+    <div>
+      Quiz
+    </div>
+  )
+}
+
+export default Quiz
