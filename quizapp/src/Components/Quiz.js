3459610f643ddfe3c5f2d3c477eb65e579c93ec7
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-01-14 03:22:21 +0530

    add quiz bank

--- quizapp/src/Components/Quiz.js
+++ quizapp/src/Components/Quiz.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Questions} from '../Helpers/QuizBank'
 
 function Quiz() {
+  const [currQuestion, setCurrQuestion] = useState(0)
+  const [optionChosen, setOptionChosen] = useState("")
+
+
   return (
-    <div>
-      Quiz
+    <div className='Quiz'>
+      <h1>{Questions[currQuestion].prompt}</h1>
+      <div className='options'>
+        <button onClick={()=>setOptionChosen("A")}>{Questions[currQuestion].optionA}</button>
+        <button onClick={()=>setOptionChosen("B")}>{Questions[currQuestion].optionB}</button>
+        <button onClick={()=>setOptionChosen("C")}>{Questions[currQuestion].optionC}</button>
+        <button onClick={()=>setOptionChosen("D")}>{Questions[currQuestion].optionD}</button>
+      </div>
+      <button>Next Question</button>
     </div>
   )
 }
---
+++ quizapp/src/Helpers/QuizBank.js
@@ -0,0 +1,28 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3?",
+        optionA: "13",
+        optionB: "23",
+        optionC: "4",
+        optionD: "2",
+        answer: "C"
+    },
+
+    {
+        prompt: "What is 3-1?",
+        optionA: "13",
+        optionB: "23",
+        optionC: "4",
+        optionD: "2",
+        answer: "D"
+    },
+
+    {
+        prompt: "What is 10+3?",
+        optionA: "13",
+        optionB: "23",
+        optionC: "4",
+        optionD: "2",
+        answer: "A"
+    }
+];
