62b73e5564fe1aca059c7620277ee6cc419d2a9b
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-01-14 03:55:11 +0530

    add score fcns

--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -8,11 +8,13 @@ import { QuizContext } from './Helpers/Contexts';
 
 function App() {
   const [gameState, setGameState] = useState("menu");
+  const [score, setScore] =useState(0)
+
   return (
     <div className="App">
       <h1>Quiz App</h1>
 
-      <QuizContext.Provider value={{gameState, setGameState}}>
+      <QuizContext.Provider value={{gameState, setGameState, score, setScore}}>
       {gameState === "menu" && <MainMenu />}
       {gameState === "quiz" && <Quiz />}
       {gameState === "endScreen" && <EndScreen />}
--- quizapp/src/Components/Quiz.js
+++ quizapp/src/Components/Quiz.js
@@ -1,11 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {QuizContext} from '../Helpers/Contexts'
 import {Questions} from '../Helpers/QuizBank'
 
 function Quiz() {
+  const {score, setScore} = useContext(QuizContext)
   const [currQuestion, setCurrQuestion] = useState(0)
   const [optionChosen, setOptionChosen] = useState("")
 
-
+  const nextQuestion = ()=>{
+    if (Questions[currQuestion].answer == optionChosen){
+      setScore(score+1)
+    }
+    setCurrQuestion(currQuestion+1)
+  }
   return (
     <div className='Quiz'>
       <h1>{Questions[currQuestion].prompt}</h1>
@@ -15,7 +22,14 @@ function Quiz() {
         <button onClick={()=>setOptionChosen("C")}>{Questions[currQuestion].optionC}</button>
         <button onClick={()=>setOptionChosen("D")}>{Questions[currQuestion].optionD}</button>
       </div>
-      <button>Next Question</button>
+
+      {currQuestion==Questions.length-1?(
+        <button>Finish Quiz</button>
+      ) : (
+        <button onClick={nextQuestion}>Next Question</button>
+      )
+      }
+      
     </div>
   )
 }
