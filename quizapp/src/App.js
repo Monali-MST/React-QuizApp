addddbe3a85e1cb5c2e46b3ec06e38c56d98b393
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-01-14 01:34:16 +0530

    link game states

--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import MainMenu from './Components/MainMenu';
 import Quiz from './Components/Quiz';
 import EndScreen from './Components/EndScreen';
+
+import { QuizContext } from './Helpers/Contexts';
 
 function App() {
   const [gameState, setGameState] = useState("menu");
@@ -10,9 +12,11 @@ function App() {
     <div className="App">
       <h1>Quiz App</h1>
 
+      <QuizContext.Provider value={{gameState, setGameState}}>
       {gameState === "menu" && <MainMenu />}
       {gameState === "quiz" && <Quiz />}
       {gameState === "endScreen" && <EndScreen />}
+      </QuizContext.Provider>
     </div>
   );
 }
--- quizapp/src/Components/MainMenu.js
+++ quizapp/src/Components/MainMenu.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import { QuizContext } from '../Helpers/Contexts';
 
 export default function MainMenu() {
+  const {gameState, setGameState} = useContext(QuizContext);
+
   return (
-    <div>
-      MainnnnnMenu
+    <div className='Menu'>
+      <button onClick={()=>{setGameState("quiz")}}> Start Quiz</button>
     </div>
-  )
+  );
 }
